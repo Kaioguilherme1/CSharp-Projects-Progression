1620a670875532d2c553333d96e972899ebb8277
kaio Guilherme <kaioguilherme444@gmail.com>
2024-03-27 22:39:20 -0400

    1. Calculator v 1.0.0
    Project number one completed

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,102 @@
+﻿//author: Kaio Guilherme
+//date: 2024-03-27
+using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    class Calculator
+    {   
+        private List<String> history = new List<String>();
+        
+        private void AddToHistory(string operation, int num, double num1, double num2, double result)
+        {
+            this.history.Add($"{num}. | {num1} {operation} {num2} = {result}");
+        }
+        public static void Main(string[] args)
+        {
+            int operation, count = 0;
+            double num1, num2, result;
+            num1 = num2 = 0;
+            Calculator calculator = new Calculator();
+            
+            do
+            {
+                count++;
+                Console.Clear();
+                Console.WriteLine("|==========================================|");
+                Console.WriteLine("|   Welcome to the Calculator Application! |");
+                Console.WriteLine("|==========================================|");
+                Console.WriteLine("| 1. Addition                              |");
+                Console.WriteLine("| 2. Subtraction                           |");
+                Console.WriteLine("| 3. Multiplication                        |");
+                Console.WriteLine("| 4. Division                              |");
+                Console.WriteLine("| 5. History                               |");
+                Console.WriteLine("| 0. Exit                                  |");
+                Console.WriteLine("|==========================================|");
+                Console.Write("Enter the operation you want to perform: >> ");
+                operation = Convert.ToInt32(Console.ReadLine());
+            
+                if (operation != 0 && operation != 5)
+                {
+                    Console.Write("Enter the first number >>> ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Enter the second number >>> ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+                }
+
+                switch (operation)
+                {   
+                    case 1:
+                        result = num1 + num2;
+                        Console.WriteLine($"The sum of {num1} and {num2} is {result}");
+                        // Aqui você precisa criar uma instância da classe Calculator para chamar o método AddToHistory
+                        calculator.AddToHistory("+",count, num1, num2, result);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        result = num1 - num2;
+                        Console.WriteLine($"The difference of {num1} and {num2} is {result}");
+                        // Aqui você precisa criar uma instância da classe Calculator para chamar o método AddToHistory
+                        calculator.AddToHistory("-",count , num1, num2, result);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        result = num1 * num2;
+                        Console.WriteLine($"The product of {num1} and {num2} is {result}");
+                        // Aqui você precisa criar uma instância da classe Calculator para chamar o método AddToHistory
+                        calculator.AddToHistory("*",count , num1, num2, result);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        result = num1 / num2;
+                        Console.WriteLine($"The division of {num1} and {num2} is {result}");
+                        // Aqui você precisa criar uma instância da classe Calculator para chamar o método AddToHistory
+                        calculator.AddToHistory("/",count, num1, num2, result);
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        count--;
+                        Console.Clear();
+                        Console.WriteLine("|=========================================|");
+                        Console.WriteLine("|         History of operations           |");
+                        Console.WriteLine("|=========================================|");
+                        foreach (String operation_history in calculator.history)
+                        {
+                            Console.WriteLine(operation_history);
+                        }
+
+                        Console.ReadLine();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting the application...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation. Please try again.");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (operation != 0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,150 @@
+# CSharp-Projects-Progression
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/Kaioguilherme1/CSharp-Projects-Progression/blob/main/LICENSE)
+[![C#](https://img.shields.io/badge/C%23-Projects-blue)](https://github.com/Kaioguilherme1/CSharp-Projects-Progression)
+[![.NET](https://img.shields.io/badge/.NET-Projects-blue)](http://dotnet.microsoft.com/)
+[![ASP.NET](https://img.shields.io/badge/ASP.NET-Projects-blue)](http://asp.net/)
+[![Blazor](https://img.shields.io/badge/Blazor-Projects-blue)](http://blazor.net/)
+[![MAUI](https://img.shields.io/badge/MAUI-Projects-blue)](http://maui.net/)
+
+
+
+## Overview
+
+This Projects presents a list of 10 progressive projects in C# for learning, covering from basic concepts to more advanced challenges. The projects were selected to provide a variety of practical experiences, allowing learners to develop their programming skills using different technologies, including .NET MAUI, [ASP.NET](http://asp.net/) MVC, [ASP.NET](http://asp.net/) Core, and Blazor.
+
+## Project List
+
+### 1. Simple Calculator
+
+---
+
+### Description
+
+Develop a simple calculator that can perform basic operations such as addition, subtraction, multiplication, and division.
+
+### Technology Used
+
+C# (Console Application)
+
+### 2. Hangman Game
+
+---
+
+### Description
+
+Create the classic hangman game where the player must guess the hidden word before running out of attempts.
+
+### Technology Used
+
+C# (Console Application)
+
+### 3. Unit Converter
+
+---
+
+### Description
+
+Implement a unit converter that can convert between different units of measurement, such as temperature, length, weight, etc.
+
+### Technology Used
+
+C# (Console Application)
+
+### 4. Contacts Management Application
+
+---
+
+### Description
+
+Develop an application that allows the user to add, edit, view, and delete contacts.
+
+### Technology Used
+
+.NET MAUI
+
+### 5. Task List
+
+---
+
+### Description
+
+Create a task list application where the user can add, remove, and mark tasks as completed.
+
+### Technology Used
+
+.NET MAUI
+
+### 6. Inventory Management App
+
+---
+
+### Description
+
+Develop an application for inventory management that can add, edit, view, and delete products from the inventory.
+
+### Technology Used
+
+[ASP.NET](http://asp.net/) MVC
+
+### 7. Password Manager and Generator
+
+---
+
+### Description
+
+Develop an application that can generate secure passwords and store them securely. Additionally, the application should allow the user to view and manage their passwords with features such as adding, editing, and deleting passwords.
+
+### Technology Used
+
+.NET MAUI
+
+### 8. Blog Application
+
+---
+
+### Description
+
+Create a blog application where users can create, edit, view, and delete posts.
+
+### Technology Used
+
+[ASP.NET](http://asp.net/) Core MVC
+
+### 9. Real-time Chat Application
+
+---
+
+### Description
+
+Develop a real-time chat application using SignalR to allow communication between multiple users.
+
+### Technology Used
+
+[ASP.NET](http://asp.net/) Core with SignalR
+
+### 10. E-commerce Platform
+
+---
+
+### Description
+
+Implement a complete e-commerce platform, including product listing, shopping cart, checkout, and order management.
+
+### Technology Used
+
+[ASP.NET](http://asp.net/) Core with Blazor (or Razor Pages)
+
+## Conclusion
+
+This list of C# projects offers a natural progression of challenges, providing an ideal opportunity for learners to develop their programming skills in a practical and stimulating environment. Each project addresses different aspects of C# programming, allowing for a comprehensive and in-depth understanding of the language and its associated technologies.
+
+## ✒️ Authors
+
+Mention all those who helped lift the project from its inception
+
+* **developer** - *Initial Work* - [Kaio Guilherme](https://github.com/Kaioguilherme1)
+
+## 📑 License
+
+This project is under license (MIT) - see the file [LICENSE.md](https://github.com/Kaioguilherme1/CSharp-Projects-Progression/blob/main/LICENSE) for details.
